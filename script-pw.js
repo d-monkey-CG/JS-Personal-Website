515979c29a786784cf88c456c4f6d9ccd054a36f
d-monkey-CG <138995840+d-monkey-CG@users.noreply.github.com>
2025-08-12 11:49:51 -0500

    Update script-pw.js
    added video button and playback/close functionality

--- script-pw.js
+++ script-pw.js
@@ -8,3 +8,31 @@ function toggleDetails(button) {
       paragraph.style.display = isVisible ? "none" : "block";
       button.textContent = isVisible ? "▶" : "▼";
     }
+
+// Get modal elements
+var modal = document.getElementById("videoModal");
+var btn = document.getElementById("openVideo");
+var span = document.getElementsByClassName("close")[0];
+var video = modal.querySelector("video");
+
+// Open modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  video.play();
+}
+
+// Close modal
+span.onclick = function() {
+  modal.style.display = "none";
+  video.pause();
+  video.currentTime = 0;
+}
+
+// Close if user clicks outside video
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    video.pause();
+    video.currentTime = 0;
+  }
+}
